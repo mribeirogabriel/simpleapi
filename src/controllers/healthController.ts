c26e077a1dcb59ee494cdcbd43f4324e3caf8a6e
mribeirogabriel <mribeirogabriel@hotmail.com>
2022-03-01 18:28:14 -0300

    fix deploy

--- src/controllers/healthController.ts
+++ src/controllers/healthController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 
 const APP_NAME = process.env.APP_NAME || "simpleapi";
+const ENV_NAME = process.env.ENV_NAME || "dev";
 
 export default class HealthController {
   static async healthCheck(req: Request, res: Response): Promise<any> {
     const response = {
       app: APP_NAME,
+      env: ENV_NAME,
       method: req.method,
       path: req.path,
       timestamp: Date.now(),
